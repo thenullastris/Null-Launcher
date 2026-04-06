fcfb2e22815c7b9bc99a67a0cfd65bdd857fe1be
ReaperZxMC <salonprum006@gmail.com>
2026-04-07 02:39:59 +0700

    Delete LauncherPreferencesViewController.m

--- LauncherPreferencesViewController.m
+++
@@ -1,3 +0,0 @@
-- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section {
-    return [NSString stringWithFormat:@"Zenith Launcher 2.0-development\n\nI\uOS: %@ on %@\nPID: %@\nProudly built in Cambodia 🇰🇭\nCreated by ReaperZxMC", [[UIDevice currentDevice] systemVersion], [[UIDevice currentDevice] model], [[NSProcessInfo processInfo] processIdentifier]];
-}
